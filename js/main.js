ae99c5b5bda2ed112e6e43450d9fb9a8f35f0975
MaximilianoBustios <66762728+MaximilianoBustios@users.noreply.github.com>
2022-04-07 21:58:05 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,61 @@
+const portfolioList = document.querySelector(".portfolio-list");
+
+const links = [
+  {
+    label: "Week 1",
+    url: "week1/index.html",
+  },
+  {
+    label: "Week 2",
+    url: "week2/index.html",
+  },
+  {
+    label: "Week 3",
+    url: "week3/index.html",
+  },
+  {
+    label: "Week 4",
+    url: "week4/index.html",
+  },
+  {
+    label: "Week 5",
+    url: "week5/index.html",
+  },
+  {
+    label: "Week 6",
+    url: "week6/index.html",
+  },
+  {
+    label: "Week 7",
+    url: "week7/index.html",
+  },
+  {
+    label: "Week 8",
+    url: "week8/index.html",
+  },
+  {
+    label: "Week 9",
+    url: "week9/index.html",
+  },
+  {
+    label: "Week 10",
+    url: "week10/",
+  },
+  {
+    label: "Week 11",
+    url: "week11/",
+  },
+  {
+    label: "Week 12 & 13",
+    url: "week12&13/",
+  }
+];
+
+links.forEach((element) => {
+  let li = document.createElement("li");
+  let a = document.createElement("a");
+  a.setAttribute("href", element.url);
+  a.textContent = element.label;
+  li.appendChild(a);
+  portfolioList.appendChild(li);
+});
